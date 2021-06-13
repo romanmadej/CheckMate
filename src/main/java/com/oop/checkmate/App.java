07bd3812ffa61d2fe482d12b2a7dc64f665ef8c3
Hubert Dej <34969251+hubertdej@users.noreply.github.com>
2021-06-13 13:54:11 +0200

    add MenuView and basic navigation

--- src/main/java/com/oop/checkmate/App.java
+++ src/main/java/com/oop/checkmate/App.java
@@ -2,23 +2,52 @@ package com.oop.checkmate;
 
 import static com.oop.checkmate.Constants.SQUARE_SIZE;
 
-import com.oop.checkmate.controller.BoardController;
+import java.io.IOException;
+
+import com.oop.checkmate.view.BoardView;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class App extends Application {
 	@Override
-	public void start(Stage stage) throws ClassNotFoundException {
+	public void start(Stage stage) throws ClassNotFoundException, IOException {
 		Class.forName("com.oop.checkmate.model.engine.MagicBitboards");
-		int size = 8 * SQUARE_SIZE;
-		BoardController boardController = new BoardController(true);
-		Scene scene = new Scene(boardController.getView(), size, size);
+
+		Scene scene = Navigator.createNamedScene("/MenuView.fxml", null);
 		stage.setTitle("CheckMate");
 		stage.setScene(scene);
+		stage.setWidth(8 * SQUARE_SIZE);
+		stage.setHeight(6 * SQUARE_SIZE);
 		stage.setResizable(false);
+		stage.addEventFilter(WindowEvent.WINDOW_CLOSE_REQUEST, this::closeWindowEvent);
 		stage.show();
+	}
+
+	private void closeWindowEvent(WindowEvent event) {
+		Stage stage = (Stage) event.getSource();
+		if (!(stage.getScene().getRoot() instanceof BoardView)) {
+			return;
+		}
+		event.consume();
+
+		Alert confirmationAlert = new Alert(Alert.AlertType.CONFIRMATION,
+				"Quit to the main menu? Your game will be lost.", ButtonType.YES, ButtonType.NO);
+
+		confirmationAlert.showAndWait();
+		if (confirmationAlert.getResult() == ButtonType.YES) {
+			try {
+				Navigator.of(stage).setNamed("/MenuView.fxml", null);
+				stage.setHeight(6 * SQUARE_SIZE);
+				stage.setY(stage.getY() + SQUARE_SIZE);
+			} catch (Exception e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/oop/checkmate/controller/MenuController.java
@@ -0,0 +1,81 @@
+package com.oop.checkmate.controller;
+
+import static com.oop.checkmate.Constants.SQUARE_SIZE;
+
+import java.io.IOException;
+import java.util.Map;
+
+import com.oop.checkmate.Navigator;
+import com.oop.checkmate.view.BoardView;
+
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import jdk.jshell.spi.ExecutionControl;
+
+public class MenuController extends BasicController {
+	@FXML
+	private ImageView logoImageView;
+	@FXML
+	private Button playHotseatButton;
+	@FXML
+	private Button playComputerButton;
+	@FXML
+	private Button exitButton;
+
+	@Override
+	public void initialize(Map<String, Object> args) {
+		logoImageView.setFitWidth(SQUARE_SIZE * 4);
+		playHotseatButton.prefWidthProperty().bind(playComputerButton.widthProperty());
+		exitButton.prefWidthProperty().bind(playComputerButton.widthProperty());
+	}
+
+	private void navigateToBoardView(Stage stage, BoardView boardView) {
+		Navigator.of(stage).set(boardView);
+		double titleBarHeight = stage.getHeight() - stage.getScene().getHeight();
+		stage.setHeight(SQUARE_SIZE * 8 + titleBarHeight);
+		stage.setY(stage.getY() - SQUARE_SIZE);
+		stage.setResizable(false);
+	}
+
+	@FXML
+	private void playHotseatButtonOnClicked(MouseEvent mouseEvent) {
+		if (mouseEvent.getSource() != playHotseatButton) {
+			throw new UnsupportedOperationException("Incorrect button assigned");
+		}
+		if (mouseEvent.getButton() != MouseButton.PRIMARY) {
+			return;
+		}
+		Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+		BoardController boardController = new BoardController(false);
+		navigateToBoardView(stage, (BoardView) boardController.getView());
+	}
+
+	@FXML
+	private void playComputerButtonOnClicked(MouseEvent mouseEvent) {
+		if (mouseEvent.getSource() != playComputerButton) {
+			throw new UnsupportedOperationException("Incorrect button assigned");
+		}
+		if (mouseEvent.getButton() != MouseButton.PRIMARY) {
+			return;
+		}
+		Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+		BoardController boardController = new BoardController(true);
+		navigateToBoardView(stage, (BoardView) boardController.getView());
+	}
+
+	public void exitButtonOnClicked(MouseEvent mouseEvent) {
+		if (mouseEvent.getSource() != exitButton) {
+			throw new UnsupportedOperationException("Incorrect button assigned");
+		}
+		if (mouseEvent.getButton() != MouseButton.PRIMARY) {
+			return;
+		}
+		Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+		stage.close();
+	}
+}
